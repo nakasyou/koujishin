7d2effb20c5a2bef0bd602cbec3b9608cd4c931e
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-07-31 20:04:34 +0900

    indexes

---
+++ routes/api/dict/get-indexes.ts
@@ -0,0 +1,10 @@
+import { getIndexes } from "~/dict/mod.ts"
+import { HandlerContext } from "$fresh/server.ts"
+
+export const handler = async (_req: Request, _ctx: HandlerContext): Response => {
+  const result = []
+  for await (const name of getIndexes()) {
+    result.push(name)
+  }
+  return new Response(JSON.stringify(result))
+}
