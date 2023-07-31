18135184d09c0ba20a51c06b60998f37db2cc3f8
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-07-31 20:07:44 +0900

    Update get-indexes.ts

--- dict/src/get-indexes.ts
+++ dict/src/get-indexes.ts
@@ -2,6 +2,6 @@ import { expandGlob } from "$std/fs/mod.ts"
 
 export async function *getIndexes() {
   for await (const entry of expandGlob('./dict/data/**/index.yaml')) {
-    yield entry.file
+    yield entry
   }
 }
