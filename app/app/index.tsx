11e5b3661b8d9df23e22caeee6c6e26aad7b59ce
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-07-31 20:17:22 +0900

    indexes

--- app/app/index.tsx
+++ app/app/index.tsx
@@ -1,8 +1,24 @@
 import Navbar from "./components/Navbar.tsx"
+import { signal } from "@preact/signals"
+import { useEffect } from "preact/hooks"
 
 export default () => {
+  const indexes = signal<{
+    name: string
+  }[]>([])
+  useEffect(() => {
+    ;(async () => {
+      const indexesApiResult = await fetch("/api/dict/get-indexes").then(res => res.json())
+      indexes.value = indexesApiResult
+    })()
+  }, [])
   return <>
-    <div>This is app</div>
+    <div>
+      <div>This is app</div>
+      <div>
+        {JSON.stringify(indexes)}
+      </div>
+    </div>
     <div>
       <Navbar />
     </div>
