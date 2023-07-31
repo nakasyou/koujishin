4811927f4672a2648f53f1ea8912a62ef0f4e094
nakasyou <syoutarou1103@outlook.jp>
2023-07-31 11:45:25 +0000

    Gen

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -4,11 +4,12 @@
 
 import * as $0 from "./routes/_404.tsx";
 import * as $1 from "./routes/_app.tsx";
-import * as $2 from "./routes/api/dict/get-indexes.ts";
-import * as $3 from "./routes/api/joke.ts";
-import * as $4 from "./routes/app/index.tsx";
-import * as $5 from "./routes/greet/[name].tsx";
-import * as $6 from "./routes/index.tsx";
+import * as $2 from "./routes/api/dict/get-data-from-word/[word].ts";
+import * as $3 from "./routes/api/dict/get-indexes.ts";
+import * as $4 from "./routes/api/joke.ts";
+import * as $5 from "./routes/app/index.tsx";
+import * as $6 from "./routes/greet/[name].tsx";
+import * as $7 from "./routes/index.tsx";
 import * as $$0 from "./islands/App.tsx";
 import * as $$1 from "./islands/Counter.tsx";
 
@@ -16,11 +17,12 @@ const manifest = {
   routes: {
     "./routes/_404.tsx": $0,
     "./routes/_app.tsx": $1,
-    "./routes/api/dict/get-indexes.ts": $2,
-    "./routes/api/joke.ts": $3,
-    "./routes/app/index.tsx": $4,
-    "./routes/greet/[name].tsx": $5,
-    "./routes/index.tsx": $6,
+    "./routes/api/dict/get-data-from-word/[word].ts": $2,
+    "./routes/api/dict/get-indexes.ts": $3,
+    "./routes/api/joke.ts": $4,
+    "./routes/app/index.tsx": $5,
+    "./routes/greet/[name].tsx": $6,
+    "./routes/index.tsx": $7,
   },
   islands: {
     "./islands/App.tsx": $$0,
