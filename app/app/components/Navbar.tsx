700c6fe532cca648700c2df2aad818c41211a9f5
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-07-31 17:25:44 +0900

    Create Navbar.tsx

---
+++ app/app/components/Navbar.tsx
@@ -0,0 +1,15 @@
+export default () => {
+  return <div class="fixed bottom-0 left-0 bg-slate-50 dark:bg-slate-700 drop-shadow-lg rounded-full w-full p-4 m-4">
+          <div class="flex">
+            <div>
+              <span>広辞深 Alpha</span>
+            </div>
+            <div>
+              <select>
+                <option value="yoko">ヨコ</option>
+                <option value="tate">タテ</option>
+              </select>
+            </div>
+          </div>
+        </div>
+}
