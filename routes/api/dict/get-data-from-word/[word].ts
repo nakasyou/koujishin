a0648b782a8278382e344ac071f65b04a4fe9d3e
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-07-31 20:44:00 +0900

    Create [word].ts

---
+++ routes/api/dict/get-data-from-word/[word].ts
@@ -0,0 +1,7 @@
+import { getDataFromIndex } from "~/dict/mod.ts"
+import { HandlerContext } from "$fresh/server.ts"
+
+export const handler = async (_req: Request, ctx: HandlerContext): Response => {
+  const result = await getDataFromIndex(ctx.params.word)
+  return new Response(JSON.stringify(result))
+}
