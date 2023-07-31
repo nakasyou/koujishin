c566c63aea8d08b2137486bab019699328251fe5
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-07-31 16:44:11 +0900

    Tailwind v3

--- twind.config.ts
+++ twind.config.ts
@@ -1,5 +1,11 @@
-import { Options } from "$fresh/plugins/twind.ts";
+import { Options } from "$fresh/plugins/twindv1.ts";
+
+import { defineConfig, Preset } from "@twind/core";
+import presetTailwind from "https://esm.sh/@twind/preset-tailwind@1.1.4";
 
 export default {
+  ...defineConfig({
+    presets: [presetTailwind() as Preset],
+  }),
   selfURL: import.meta.url,
 } as Options;
