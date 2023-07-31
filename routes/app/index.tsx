21ca6e1ef1d685d96747cdbd20bd826d0e265cff
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-07-31 17:23:20 +0900

    Update index.tsx

--- routes/app/index.tsx
+++ routes/app/index.tsx
@@ -1,10 +1,9 @@
 import { Head } from "$fresh/runtime.ts"
+import App from "~/islands/App.tsx"
 
 export default () => (<>
   <Head>
     <title>App / 広辞深</title>
   </Head>
-  <div>
-    
-  </div>
+  <App />
 </>)
