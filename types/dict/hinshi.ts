80c156a1145a06937fe8467382616867d7249e0c
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-08-02 13:49:11 +0900

    fix

---
+++ types/dict/hinshi.ts
@@ -0,0 +1,36 @@
+// 形容動詞とかの概念が英語にないので、動詞もVerbとせずに、Doushiとする
+
+interface HinshiBase {
+  /**
+   * 品詞の種類
+   */
+  type: "普通名詞" | "固有名詞" | "動詞" | "形容動詞"
+}
+interface Doushi extends HinshiBase {
+  type: "動詞"
+  /**
+   * 活用の行。
+   * @remarks
+   * 「信じる」はザ行上一段活用。よって「ザ」になる。
+   */
+  conjugationColumn: "ア" | "カ" | "ガ" | "サ" | "ザ" // めんどくさいので随時追加
+  /**
+   * 活用の段。
+   * @remarks
+   * 「信じる」はザ行上一段活用。よって「上一段」になる。
+   */
+  conjugationType: "五段" | "上一段" | "下一段"
+  /**
+   * 自動詞か他動詞か。中学文法で習ったはず
+   */
+  jitaDoushi: "自" | "他"
+}
+interface Meishi extends HinshiBase {
+  type: "普通名詞" | "固有名詞"
+}
+export type Hinshi =
+  (
+    Doushi |
+    Meishi
+  ) 
+  & HinshiBase
