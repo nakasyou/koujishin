32bd256afafc0498963c3d66b4369f61ad9b8d7c
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-08-03 13:35:23 +0900

    Add word: 楽しい

---
+++ dict/data/た/の/たのしい/楽しい/index.ts
@@ -0,0 +1,19 @@
+import { defineWordData } from "~/types/dict.ts"
+
+export default defineWordData({
+  read: "たのし-い",
+  kanji: "楽しい",
+  title: "楽しい",
+  parts: [
+    {
+      hinshis: [{
+        type: "形容詞"
+      }],
+      meanings: [
+        {
+          body: "心が晴々しているさま。心地よく明るい気分。",
+        },
+      ]
+    }
+  ]
+})
