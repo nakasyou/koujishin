41b8e30eeda48f376eda8ab575601f2e285d07ba
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-08-03 11:02:31 +0900

    TSに転換

---
+++ dict/data/し/ゅ/しゅうきょう/州境/index.ts
@@ -0,0 +1,21 @@
+import { defineWordData } from "~/types/dict.ts"
+
+export default defineWordData({
+  read: "しゅうきょう",
+  kanji: "宗教",
+  title: "宗教",
+  parts: [
+    {
+      hinshi: {
+        type: "普通名詞"
+      },
+      meanings: [
+        {
+          body: "州の境界。",
+          examples: [],
+          images: []
+        }
+      ]
+    }
+  ]
+})
