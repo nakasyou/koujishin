bfb08039d793d93254e31423e3f43595b4f06c23
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-08-03 12:15:01 +0900

    Add word: 数字

---
+++ dict/data/す/う/すうじ/数字/index.ts
@@ -0,0 +1,21 @@
+import { defineWordData } from "~/types/dict.ts"
+
+export default defineWordData({
+  read: "すうじ",
+  kanji: "数字",
+  title: "数字",
+  parts: [
+    {
+      hinshis: [{
+        type: "普通名詞"
+      }],
+      meanings: [
+        {
+          body: "数を表す記号及び文字のこと。",
+          examples: [],
+          images: []
+        },
+      ]
+    }
+  ]
+})
