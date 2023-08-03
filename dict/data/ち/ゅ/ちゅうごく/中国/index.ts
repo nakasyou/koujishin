58a1aff1d7ac13d509a44f568072bc471dc9c17f
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-08-03 12:12:14 +0900

    Add word: 中国

---
+++ dict/data/ち/ゅ/ちゅうごく/中国/index.ts
@@ -0,0 +1,26 @@
+import { defineWordData } from "~/types/dict.ts"
+
+export default defineWordData({
+  read: "ちゅうごく",
+  kanji: "中国",
+  title: "中国",
+  parts: [
+    {
+      hinshis: [{
+        type: "固有名詞"
+      }],
+      meanings: [
+        {
+          body: "東アジアの国、中華人民共和国のこと。",
+          examples: [],
+          images: []
+        },
+        {
+          body: "日本の地方の一つ。中国地方。",
+          examples: [],
+          images: [],
+        }
+      ]
+    }
+  ]
+})
