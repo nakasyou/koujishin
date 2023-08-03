55f1d0a23723d075db35f9a08ed00bad030cc58c
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-08-03 10:38:26 +0900

    批判が悪い？

--- app/app/index.tsx
+++ app/app/index.tsx
@@ -18,7 +18,6 @@ export default () => {
         const json5Text = await fetch(`/api/dict/get-data-from-word?id=${index.id}`).then(res => res.text())
         const wordData: WordData = (new Function("return (" + json5Text + ")"))()
         wordDatasTmp.push(wordData)
-        alert(JSON.stringify(wordDatasTmp, null, 2))
         setWordDatas([...wordDatasTmp])
       }
     })()
