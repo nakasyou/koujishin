7adc45ec52c4b1f8e3af0696b76f3ca114db544d
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-08-03 09:51:03 +0900

    evalから、new Functionにして高速化

--- app/app/index.tsx
+++ app/app/index.tsx
@@ -16,7 +16,7 @@ export default () => {
       const wordDatasTmp: WordData[] = []
       for (const index of indexesApiResult) {
         const json5Text = await fetch(`/api/dict/get-data-from-word?id=${index.id}`).then(res => res.text())
-        const wordData: WordData = window.eval("(" + json5Text + ")")
+        const wordData: WordData = (new Function("return (" + json5Text + ")"))()
         wordDatasTmp.push(wordData)
         setWordDatas([...wordDatasTmp])
       }
