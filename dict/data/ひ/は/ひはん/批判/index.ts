bc013063a7c843d3955953ef800f6a812570658f
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-08-03 10:18:43 +0900

    fix

---
+++ dict/data/ひ/は/ひはん/批判/index.ts
@@ -0,0 +1,34 @@
+import { defineWordData } from "~/types/dict.ts"
+
+export default defineWordData({
+  read: "ひはん",
+  kanji: "批判",
+  title: "批判",
+  parts: [
+    {
+      hinshi: {
+        type: "普通名詞"
+      },
+      meanings: [
+        {
+          body: "欠点や過ちを指摘して責めたりすること。",
+          examples: [],
+          images: []
+        }
+      ]
+    },
+    {
+      hinshi: {
+        type: "動詞",
+        conjugationColumn: "サ",
+        conjugationType: "上一段",
+        jitaDoushi: "他",
+      },
+      meanings: [
+        {
+          body: "ほかの人の過ちを、責めたり、とがめたりする行為。"
+        }
+      ]
+    }
+  ]
+})
---
+++ dict/data/り/ん/りん/リン/index.ts
@@ -0,0 +1,21 @@
+import { defineWordData } from "~/types/dict.ts"
+
+export default defineWordData({
+  read: "りん",
+  kanji: "リン",
+  title: "リン",
+  parts: [
+    {
+      hinshi: {
+        type: "普通名詞"
+      },
+      meanings: [
+        {
+          body: "元素番号16番の元素。元素記号はP。窒素族元素（15族）のひとつ。周期は3。原子量は30.97。英語では、英語では、`phosphorus`。",
+          examples: [],
+          images: []
+        }
+      ]
+    }
+  ]
+})
