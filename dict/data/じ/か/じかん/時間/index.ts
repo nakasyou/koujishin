d82005285f5d0c905c1ec4a0a7d7e8ad77550cbb
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-08-03 13:09:40 +0900

    Add word: "時間"

---
+++ dict/data/じ/か/じかん/時間/index.ts
@@ -0,0 +1,33 @@
+import { defineWordData } from "~/types/dict.ts"
+
+export default defineWordData({
+  read: "じかん",
+  kanji: "時間",
+  title: "時間",
+  parts: [
+    {
+      hinshis: [{
+        type: "普通名詞"
+      }],
+      meanings: [
+        {
+          body: "ある時刻とある時刻の間の量。",
+          examples: [],
+          images: []
+        },
+        {
+          body: "時の流れ。",
+          examples: [],
+          images: [],
+        },
+        {
+          body: "ときの軸の上の、ある一点のこと。",
+          examples: [
+            "もう休憩の時間だ。"
+          ],
+          images: [],
+        }
+      ]
+    }
+  ]
+})
