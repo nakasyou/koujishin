57131e5932411960a2fe48476cf8872fe0be3882
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-08-03 12:08:07 +0900

    Add word:

---
+++ dict/data/に/ほ/にほん/日本/index.ts
@@ -0,0 +1,21 @@
+import { defineWordData } from "~/types/dict.ts"
+
+export default defineWordData({
+  read: "にほん",
+  kanji: "日本",
+  title: "日本",
+  parts: [
+    {
+      hinshis: [{
+        type: "普通名詞"
+      }],
+      meanings: [
+        {
+          body: "日本国のこと。",
+          examples: [],
+          images: []
+        }
+      ]
+    }
+  ]
+})
