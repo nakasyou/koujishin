2242f02393d20577d1f91618bcdb17bc30c4ca76
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-08-03 11:12:59 +0900

    [Fix typo]: buttomをbottomの

--- routes/index.tsx
+++ routes/index.tsx
@@ -30,7 +30,7 @@ export default function Home() {
                 </div>
               </div>
             </div>
-            <div class="fixed buttom-0 left-0">
+            <div class="fixed bottom-0 left-0">
               <a href="/app" class="bg-teal-400 drop-shadow-md rounded-lg hover:bg-teal-500 p-4">広辞深を開く↗︎</a>
             </div>
           </div>
