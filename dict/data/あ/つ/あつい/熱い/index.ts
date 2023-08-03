b0fc0d4e38fe98727c3dc30a4d0bcf7c9b49e858
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-08-03 13:25:50 +0900

    Add word: 熱い

---
+++ dict/data/あ/つ/あつい/熱い/index.ts
@@ -0,0 +1,38 @@
+import { defineWordData } from "~/types/dict.ts"
+
+export default defineWordData({
+  read: "あつ-い",
+  kanji: "熱い",
+  title: "熱い",
+  parts: [
+    {
+      hinshis: [{
+        type: "形容詞"
+      }],
+      meanings: [
+        {
+          body: "その物の温度がかなり高い状況。",
+          examples: [],
+          images: []
+        },
+        {
+          body: "恋人同士の仲が、目立ってよい状況。",
+          examples: [],
+          images: [],
+        },
+        {
+          body: "情熱的な状況。",
+          examples: [],
+          images: [],
+        },
+        {
+          body: "人気がある。",
+          examples: [
+            "このレポジトリは最近熱い。"
+          ],
+          images: [],
+        },
+      ]
+    }
+  ]
+})
