6d3b9e378f42a578f464e6bce75ceaa797ab630d
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-08-03 11:14:47 +0900

    fix

--- routes/index.tsx
+++ routes/index.tsx
@@ -30,9 +30,11 @@ export default function Home() {
                 </div>
               </div>
             </div>
-            <div class="fixed bottom-0 left-0">
-              <a href="/app" class="bg-teal-400 drop-shadow-md rounded-lg hover:bg-teal-500 p-4">広辞深を開く↗︎</a>
-            </div>
+          </div>
+        </div>
+        <div class="">
+          <div class="flex justify-center">
+            <a href="/app" class="bg-teal-400 drop-shadow-md rounded-lg hover:bg-teal-500 p-4">広辞深を開く↗︎</a>
           </div>
         </div>
       </main>
