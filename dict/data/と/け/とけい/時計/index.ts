329d3fd30951ac18c55ff103027fefb484df977d
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-08-03 13:06:46 +0900

    Add word: 時計

---
+++ dict/data/と/け/とけい/時計/index.ts
@@ -0,0 +1,21 @@
+import { defineWordData } from "~/types/dict.ts"
+
+export default defineWordData({
+  read: "とけい",
+  kanji: "時計",
+  title: "時計",
+  parts: [
+    {
+      hinshis: [{
+        type: "普通名詞"
+      }],
+      meanings: [
+        {
+          body: "時刻や日付を示したり、時間を測ったりする仕掛け、機械。",
+          examples: [],
+          images: []
+        }
+      ]
+    }
+  ]
+})
