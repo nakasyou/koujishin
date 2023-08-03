1432f41c9204c75a2b85ad1046685221abe040b5
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-08-03 13:27:58 +0900

    Add word: 冷たい

---
+++ dict/data/つ/め/つめたい/冷たい/index.ts
@@ -0,0 +1,28 @@
+import { defineWordData } from "~/types/dict.ts"
+
+export default defineWordData({
+  read: "つめた-い",
+  kanji: "冷たい",
+  title: "冷たい",
+  parts: [
+    {
+      hinshis: [{
+        type: "形容詞"
+      }],
+      meanings: [
+        {
+          body: "その物の温度が低い状況。",
+          examples: [],
+          images: []
+        },
+        {
+          body: "態度が冷酷なさま。",
+          examples: [
+            "最近、親友が私に対して冷たい。"
+          ],
+          images: [],
+        }
+      ]
+    }
+  ]
+})
